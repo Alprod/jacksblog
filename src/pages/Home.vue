a39fd439bbef1ba1a45fe02ed8e5dafb2591edf3
Alprod <41442860+Alprod@users.noreply.github.com>
2025-08-21 12:50:11 +0200

    refacto

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -11,14 +11,4 @@ const { articles, loading, error } = useArticles();
     <div class=" py-2">
         <Button :icon="PlusIcon" label="Ajouter" variant="primary" size="md" />
     </div>
-    <p v-if="loading">Chargement...</p>
-    <p v-if=" error" class="italic text-red-500 text-xl">{{ error }}</p>
-    <div v-if="!articles">Pas d'article pour le moment ...</div>
-    <section v-else class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        <ArticleCard
-            v-for="article in articles.data"
-            :key="article._id"
-            v-bind="article"
-        />
-    </section>
 </template>
