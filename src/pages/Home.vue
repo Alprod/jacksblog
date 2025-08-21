316b0faf91d240da4b53b253ce1ed95281fe276e
Alprod <41442860+Alprod@users.noreply.github.com>
2025-08-21 12:53:03 +0200

    refacto

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,7 @@
 <script setup>
 import Button from '@/components/ui/Button.vue';
-import { PlusIcon, CheckIcon, XMarkIcon, QuestionMarkCircleIcon, NoSymbolIcon } from '@heroicons/vue/24/outline';
-import ArticleCard from '@/components/layout/card/ArticleCard.vue';
-import { useArticles } from '@/composables/useArticles';
+import { PlusIcon } from '@heroicons/vue/24/outline';
 
-const { articles, loading, error } = useArticles();
 </script>
 <template>
     <h1 class="title font-black font-stretch-extra-expanded transition">JackyBlog</h1>
